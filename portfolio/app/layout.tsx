2c5622056f77ea6594d54e7f48b6c1c95a71e6bd
Pasindu Vinsuka <nimsara.pasindu@inivosglobal.com>
2024-02-11 16:00:36 +0530

    initial setup

---
+++ portfolio/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Pasindu Vinsuka | Portfolio",
+  description:
+    "Pasindu Vinsuka is a software engineer with a 1 year of experience.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50 text-gray-950`}>
+        <div className="-z-10 bg-[#fbe2e3] relative top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="-z-10 bg-[#dbd7fb] absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        {children}
+      </body>
+    </html>
+  );
+}
